05fb775d288375d6e20ed16f0c70a49319837bba
Enrico Sacchetti <enrico@theetrain.ca>
2023-03-17 10:37:33 -0400

    feat: add dynamic form demo

--- src/routes/dynamic-form/+page.server.js
+++ src/routes/dynamic-form/+page.server.js
@@ -1,8 +1,35 @@
 /** @type {import('./$types').Actions} */
 export const actions = {
-    addPetName: async ({request}) => {
-        const formItems = await request.formData()
-        console.log({formItems})
-        return { items: [...formItems.entries()]}
-    }
+	addPet: async ({ request }) => {
+		const formItems = await request.formData();
+		console.log({ formItems });
+
+		const items = [...formItems.entries()];
+
+		/**
+		 * The pets.
+		 */
+		const pets = [...formItems.getAll('petname[]'), ''];
+
+		return { items, pets };
+	},
+	removePet: async ({ request, url }) => {
+		const formItems = await request.formData();
+		const pet = parseInt(String(url.searchParams.get('pet')), 10) || 0;
+		console.log({ pet });
+
+		const items = [...formItems.entries()];
+		const pets = [...formItems.getAll('petname[]')];
+
+		if (pet > 0) {
+			pets.splice(pet, pet);
+		}
+
+		return { items, pets };
+	},
+	submit: async () => {
+		console.log('submitted!');
+
+		return { success: true };
+	}
 };
--- src/routes/dynamic-form/+page.svelte
+++ src/routes/dynamic-form/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { enhance } from '$app/forms';
 
+	/** @type {import('./$types').ActionData} */
 	export let form;
 	$: console.log({ form });
 </script>
@@ -11,7 +12,11 @@
 
 <h1>Dynamic form</h1>
 
-<form method="POST" use:enhance>
+{#if form?.success === true}
+	<p><strong>You did it!</strong></p>
+{/if}
+
+<form method="POST" action="?/submit" use:enhance data-sveltekit-noscroll>
 	<label>
 		Your name
 		<input name="name" type="text" value={form?.items?.name ?? ''} />
@@ -38,10 +43,26 @@
 		</label>
 	</fieldset>
 	<br />
-	<label>
-		First pet's name
-		<input type="text" name="petname-0" />
-	</label>
-	<button style="--color-link: #444;" formaction="?/addPetName">Add pet name</button>
-	<button>Submit form!</button>
+	{#each form?.pets || [''] as pet, i}
+		<label for="petname-{i}">Pet #{i + 1}'s name</label>
+		<input id="petname-{i}" type="text" name="petname[]" value={pet} />
+		<button class="remove" formaction="?/removePet&pet={i}">Remove pet #{i + 1}</button>
+	{/each}
+	<div>
+		<button style="--color-link: #444;" formaction="?/addPet">Add pet name</button>
+		<button>Submit form!</button>
+	</div>
 </form>
+
+<style>
+	.remove {
+		padding: 0.5rem;
+		--color-link: darkred;
+	}
+	input:has(+ .remove) {
+		margin-bottom: 0.25rem;
+	}
+	input + .remove {
+		margin-bottom: 1.5rem;
+	}
+</style>
