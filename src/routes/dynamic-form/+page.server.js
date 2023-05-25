b7194f3b9e26a80571b2069f44dcefabc3ff0b7a
Enrico Sacchetti <enrico@theetrain.ca>
2023-05-24 23:38:53 -0400

    feat: add debugger
    Upgrade dependencies

--- src/routes/dynamic-form/+page.server.js
+++ src/routes/dynamic-form/+page.server.js
@@ -1,36 +1,40 @@
 /** @type {import('./$types').Actions} */
 export const actions = {
 	addPet: async ({ request }) => {
-		const formItems = await request.formData();
-		console.log({ formItems });
+		const formItems = await request.formData()
+		console.log({ formItems })
 
-		const items = [...formItems.entries()];
+		const items = [...formItems.entries()]
 
 		/**
 		 * The pets.
 		 */
-		let pets = Array.from(formItems.getAll('petname[]'), (el) => el.toString());
-		pets.push('');
+		let pets = Array.from(
+			formItems.getAll('petname[]'),
+			(el) => el.toString()
+		)
+		pets.push('')
 
-		return { items, pets };
+		return { items, pets }
 	},
 	removePet: async ({ request, url }) => {
-		const formItems = await request.formData();
-		const pet = parseInt(String(url.searchParams.get('pet')), 10) || 0;
-		console.log({ pet });
+		const formItems = await request.formData()
+		const pet =
+			parseInt(String(url.searchParams.get('pet')), 10) || 0
+		console.log({ pet })
 
-		const items = [...formItems.entries()];
-		const pets = [...formItems.getAll('petname[]')];
+		const items = [...formItems.entries()]
+		const pets = [...formItems.getAll('petname[]')]
 
 		if (pet > 0) {
-			pets.splice(pet, pet);
+			pets.splice(pet, pet)
 		}
 
-		return { items, pets };
+		return { items, pets }
 	},
 	submit: async () => {
-		console.log('submitted!');
+		console.log('submitted!')
 
-		return { success: true };
+		return { success: true }
 	}
-};
+}
